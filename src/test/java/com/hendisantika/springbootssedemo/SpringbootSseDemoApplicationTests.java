ff5d21595faca1b09da0485a7feea8241870a131
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-11 06:40:47 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/springbootssedemo/SpringbootSseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootssedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
