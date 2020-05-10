a395dc8f82da52378415e84cfa750207182e21af
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-11 06:45:43 +0700

    #6 - Add StockPriceService

---
+++ src/main/java/com/hendisantika/springbootssedemo/service/StockPriceService.java
@@ -0,0 +1,22 @@
+package com.hendisantika.springbootssedemo.service;
+
+import com.hendisantika.springbootssedemo.util.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-sse-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/05/20
+ * Time: 06.45
+ */
+@Service
+public class StockPriceService {
+
+    @Autowired
+    private Utils utilities;
+
+}
