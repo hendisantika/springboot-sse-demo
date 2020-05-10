3365a55e14a6ef82600d4ad36e2065a3ee16a1cf
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-11 06:44:45 +0700

    #5 - Add getStatus function in Utils

--- src/main/java/com/hendisantika/springbootssedemo/util/Utils.java
+++ src/main/java/com/hendisantika/springbootssedemo/util/Utils.java
@@ -3,6 +3,7 @@ package com.hendisantika.springbootssedemo.util;
 import org.springframework.stereotype.Component;
 
 import java.text.DecimalFormat;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +22,11 @@ public class Utils {
         DecimalFormat decimalFormat = new DecimalFormat(pattern);
         String format = decimalFormat.format((Math.random() * ((max - min) + 1)) + min);
         return format;
+    }
+
+    public String getStatus() {
+        int i = ThreadLocalRandom.current().nextInt(0, 1 + 1);
+        String[] status = {"HIGH", "LOW"};
+        return status[i];
     }
 }
