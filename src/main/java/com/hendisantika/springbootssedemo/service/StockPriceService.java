2d3ebef2a66ccb9a15f2348be7f6d1670d725249
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-11 06:46:35 +0700

    #7 - Add getStockPriceData & #7 - Add getStockPriceData &setStockPrice StockPriceService function in StockPriceService

--- src/main/java/com/hendisantika/springbootssedemo/service/StockPriceService.java
+++ src/main/java/com/hendisantika/springbootssedemo/service/StockPriceService.java
@@ -1,8 +1,15 @@
 package com.hendisantika.springbootssedemo.service;
 
+import com.hendisantika.springbootssedemo.model.StockPrice;
 import com.hendisantika.springbootssedemo.util.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +26,19 @@ public class StockPriceService {
     @Autowired
     private Utils utilities;
 
+    public Flux<List<StockPrice>> getStockPriceData(List<StockPrice> stockPriceList) {
+
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(3));
+        interval.subscribe((i) -> stockPriceList.forEach(stock -> setStockPrice(stock)));
+        Flux<List<StockPrice>> transactionFlux = Flux.fromStream(Stream.generate(() -> stockPriceList));
+        return Flux.zip(interval, transactionFlux).map(Tuple2::getT2);
+    }
+
+    private StockPrice setStockPrice(StockPrice stock) {
+        stock.setPrice(utilities.getRandomDoubleBetweenRange(1000, 5000));
+        stock.setValue(utilities.getRandomDoubleBetweenRange(1000, 5000));
+        stock.setChange(utilities.getRandomDoubleBetweenRange(10, 50));
+        stock.setStatus(utilities.getStatus());
+        return stock;
+    }
 }
